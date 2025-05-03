04361b01a46991863596b6fe86a25c5db2482bbd
AbdelilahChahbouni <abdelchahbouni@live.fr>
2025-05-03 22:05:22 +0100

    add static files and setup frontend

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .views import *
 
-# Create your views here.
+
+def home(request):
+    return render(request , "home/home.html" , {})
+
+
--- src/project/urls.py
+++ src/project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('transaction/', include('transaction.urls')),
     path('', include('home.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
