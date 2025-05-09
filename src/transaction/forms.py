d9b9000e2656bb28c528caf2b2e0dac03bb384af
AbdelilahChahbouni <abdelchahbouni@live.fr>
2025-05-09 18:42:35 +0100

    create all forms for purchase section

--- src/transaction/forms.py
+++ src/transaction/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from .models import *
-
-
+from inventory.models import InventoryStock
+from django.forms import formset_factory
 
 
 class SupplierForm(forms.ModelForm):
@@ -10,4 +10,31 @@ class SupplierForm(forms.ModelForm):
         fields = ['name' , 'phone' , 'address' , 'gstin' , 'email']
 
 
-        
+class SelectSuplierForm(forms.ModelForm):
+    def __init__(self,*args , **kwargs):
+        super().__init__(*args , **kwargs)
+        self.fields['suplier'].queryset = Supplier.objects.filter(is_deleted=False)
+        self.fields['suplier'].widget.attrs.update({'class':'textinput form-control'})
+    class Meta:
+        model = PurchaseBill
+        fields = ['suplier']
+
+class PurchaseItemForm(forms.ModelForm):
+    def __init__(self,*args , **kwargs):
+        super().__init__(*args , **kwargs)
+        self.fields['stock'].queryset =InventoryStock.objects.filter(is_deleted=False)
+        self.fields['stock'].widget.attrs.update({'class':'textinput form-control set_price stock', 'required':'true' })
+        self.fields['quantity'].widget.attrs.update({'class':'textinput form-control set_price quantity', 'required':'true','min':'1' })
+        self.fields['per_price'].widget.attrs.update({'class':'textinput form-control set_price per_price', 'required':'true','min':'1' })
+
+    class Meta:
+        model = PurchaseItem
+        fields = ['stock' , 'quantity' , 'per_price']
+
+PurchaseItemFormset = formset_factory(PurchaseItemForm , extra=1)
+
+
+class PurchaseBillDetails(forms.ModelForm):
+    class Meta:
+        model = PurchaseBillDetails
+        fields = ['eway', 'veh', 'destination', 'po','cgst', 'sgst', 'igst', 'cess', 'tcs']
