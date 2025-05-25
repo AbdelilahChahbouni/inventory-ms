1d33bc3f6011d9410f9994e1efd70888577bf99f
AbdelilahChahbouni <abdelchahbouni@live.fr>
2025-05-25 20:49:35 +0100

    create classes Form Sale

--- src/transaction/forms.py
+++ src/transaction/forms.py
@@ -38,3 +38,47 @@ class PurchaseDetailsForm(forms.ModelForm):
     class Meta:
         model = PurchaseBillDetails
         fields = ['eway', 'veh', 'destination', 'po','cgst', 'sgst', 'igst', 'cess', 'tcs','total']
+
+
+class SaleForm(forms.ModelForm):
+    def __init__(self , *args , **kwargs):
+        super().__init__(*args , **kwargs)
+        self.fields['name'].widget.attrs.update({'class':'textinput form-control','pattern':'[a-zA-Z\s]{1,50}','title':'Alphabets and Spaces' })
+        self.fields['phone'].widget.attrs.update({'class':'textinput form-control','maxlength':'10','pattern':'[0-9]{10}','title':'Numbers' })
+        self.fields['email'].widget.attrs.update({'class':'textinput form-control'})
+        self.fields['gstin'].widget.attrs.update({'class':'textinput form-control','maxlength':'15','pattern':'[A-Z0-9]{15}','title':'GSTIN' })
+
+    class Meta:
+        model = SaleBill
+        fields = ['name' , 'phone' , 'email' , 'gstin' , 'address']
+
+        widgets = {
+            'address': forms.Textarea(
+                attrs ={
+                    'class':'textinput form-control',
+                    'rows':'4'
+                }
+            )
+        }
+
+
+class SaleItemForm(forms.ModelForm):
+    def __init__(self , *args ,  **kwargs):
+        super().__init__(*args , **kwargs)
+        self.fields['stock'].queryset = InventoryStock.objects.filter(is_deleted=False)
+        self.fields['stock'].widget.attrs.update({'class':'textinput form-control set_price stock', 'required':'true' })
+        self.fields['quantity'].widget.attrs.update({'class':'textinput form-control set_price quantity', 'required':'true','min':'0' })
+        self.fields['per_price'].widget.attrs.update({'class':'textinput form-control set_price per_price', 'required':'true','min':'0' })
+
+    class Meta:
+        model = SaleItem
+        fields = ['stock' , 'quantity', 'per_price']
+
+
+
+SaleItemFormset = formset_factory(SaleItemForm , extra=1)
+
+class SaleDetailForm(forms.ModelForm):
+    class Meta:
+        model = SaleBillDetail
+        fields = ['eway', 'veh', 'destination', 'po','cgst', 'sgst', 'igst', 'cess', 'tcs','total']
